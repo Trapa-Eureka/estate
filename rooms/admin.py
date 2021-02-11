57a18abb009ac191b1b43039a4084e97ef581b6f
Trapa-Eureka <son199@hanmail.net>
2021-02-12 00:40:33 +0900

    add admin function

--- rooms/admin.py
+++ rooms/admin.py
@@ -50,6 +50,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
 
     list_filter = (
@@ -66,6 +67,9 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = ("^city", "^host__username")
 
     filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
 
 @admin.register(models.Photo)
