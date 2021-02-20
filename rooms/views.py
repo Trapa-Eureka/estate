7958636fb0888f5d6a41fa454937338610e5784f
Trapa-Eureka <son199@hanmail.net>
2021-02-20 20:30:49 +0900

    paging with limit, offset - use Python only

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,10 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    """ Limiting QuerySets(display list per page) """
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={"potato": all_rooms})
