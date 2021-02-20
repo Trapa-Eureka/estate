79b7e3f1266ea56dad057b04580693d3b64e87ce
Trapa-Eureka <son199@hanmail.net>
2021-02-21 08:15:10 +0900

    get_page, page, handling Exceptions

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,21 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
-    """ Django list nav """
-    page = request.GET.get("page")
+    """
+    - Django list nav
+    - concept of exception
+    - orphans
+    - paginator.page()
+    - handing Exceptions : use try, except - 무엇인가 하려 할때 exception이면 handing 가능.(15페이지에서 16페이지로 가려하면 자동으로 처음페이지로 이동(전체 페이지수가 15까지밖에 없으므로.))
+    """
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
