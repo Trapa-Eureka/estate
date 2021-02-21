c7d2373eda8cf643e203e7ac9225311c66086d4e
Trapa-Eureka <son199@hanmail.net>
2021-02-21 10:00:41 +0900

    class based views

--- rooms/views.py
+++ rooms/views.py
@@ -1,21 +1,18 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    """ 
+        concept of listview
+        : reference - http://ccbv.co.uk
+        : 문제점 - method는 보여주나 property(속성)는 안보여줌
     """
-    - Django list nav
-    - concept of exception
-    - orphans
-    - paginator.page()
-    - handing Exceptions : use try, except - 무엇인가 하려 할때 exception이면 handing 가능.(15페이지에서 16페이지로 가려하면 자동으로 처음페이지로 이동(전체 페이지수가 15까지밖에 없으므로.))
-    """
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
