57a9a9ef902d6ddf679a8b9daf8e81a3c77ccf45
Trapa-Eureka <son199@hanmail.net>
2021-02-21 15:55:00 +0900

    search

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,5 @@
-from django.views.generic import ListView
-from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -21,9 +20,14 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
